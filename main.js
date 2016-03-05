e5955d90be99e414b703ee114c61ac7272fe85a7
Ting Ting Tai <tting.git@gmail.com>
2016-03-05 13:37:37 -0800

    adding ko list

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+  // Get the modal
+  var modal = document.getElementById('myModal');
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+      modal.style.display = "block";
+  }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+  var dataItems = ko.observableArray();
+
+  ko.applyBindings({
+    newsFeedItems: dataItems
+  })
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+
+$(document).ready(function() {
+  $.get("http://jsonplaceholder.typicode.com/users", function(data, status) {
+    //todo check status
+    for (var i = 0; i < data.length; i++) {
+      dataItems.push(data[i]);
     }
-}
+  });
+});
